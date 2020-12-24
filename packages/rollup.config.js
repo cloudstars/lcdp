7b599f2567b9e34c0a1f31cded8b6dc76e9e5e4d
Qiangzaide <1131969775@qq.com>
2020-12-24 17:11:46 +0800

    fix:rollup配置文件修改

--- packages/rollup.config.js
+++ packages/rollup.config.js
@@ -1,27 +1,37 @@
+import { terser } from 'rollup-plugin-terser'; // 代码压缩
 import babel from 'rollup-plugin-babel';
-import typescript from 'rollup-plugin-typescript2';
 import tslint from 'rollup-plugin-tslint';
 import postcss from 'rollup-plugin-postcss';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+
 import pkg from './package.json';
 
-
 export default {
-    input: './src/index.ts',
-    output: {
-      file: pkg.main,
-      format: 'cjs'
-    },
-    plugins: [
-        babel(),
-        postcss({
-          extract: true,
-          sourceMap: true,
-          extensions: [".less", ".css"]
-        }),
-        //tslint(),
-        typescript()
-    ],
-    external: [
-      'react'
-    ]
-}
+  input: './src/index.ts',
+  output: {
+    file: pkg.main,
+    format: 'es',
+  },
+  plugins: [
+    babel(),
+    terser(),
+    postcss({
+      extract: true,
+      sourceMap: true,
+      extensions: ['.less', '.css'],
+    }),
+    commonjs(),
+    typescript(),
+    tslint({
+      throwOnError: true,
+      throwOnWarning: true,
+      include: ['*.ts', '*.tsx'],
+      exclude: ['node_modules/**', '*.js', '*.less', '*.css'],
+    }),
+    nodeResolve(),
+  ],
+
+  external: ['react', 'antd'],
+};
